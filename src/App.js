d2c1d36ebb9ee0f69a4f07367539ebf51de2e482
Draq109 <daniel.vazquez03@utrgv.edu>
2023-03-06 11:03:45 -0600

    Add functionality to Add/Delete button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import './styles/App.css';
-import TextField from '@mui/material/TextField';
-import { blue, red } from '@mui/material/colors';
+import { useRef, useState } from 'react';
 
-const inputProps = {
-  color: red,
-};
 
 function PersonalField() {
   return (
@@ -15,24 +11,54 @@ function PersonalField() {
       <TextInput attrName={"title"} text={"Title:"} ></TextInput>
       <TextInput attrName={"addr"} text={"Address:"} ></TextInput>
       <TextInput attrName={"email"} text={"Email:"} ></TextInput>
-      <p>Current Data:</p>
     </div>
   )
 }
 
 function EducationField() {
+  const [educationProfiles, setEducationProfiles] = useState([]);
+  const [univ, setUniv] = useState('');
+
   return (
     <div className="educationField field">
       <h2>Education</h2>
-      <TextInput attrName={"univ"} text={"University:"} ></TextInput>
+      <>
+        <label htmlFor="univ">University:</label>
+        <input key="univ" type="text" id="univ" name="univ"
+          onChange={e => setUniv(e.target.value)} />
+      </>
       <TextInput attrName={"city"} text={"City:"} ></TextInput>
-      <TextInput attrName={"degree"} text={"Degree:"} ></TextInput>
-      <TextInput attrName={"gpa"} text={"GPA:"} ></TextInput>
+      <TextInput attrName={"degree"} text={"Degree:"}></TextInput>
+      <TextInput attrName={"gpa"} text={"GPA:"}  ></TextInput>
       <TextInput attrName={"from"} text={"From:"} ></TextInput>
       <TextInput attrName={"to"} text={"To:"} ></TextInput>
       <div>
-        <button className="Add-button Education-field">Add</button>
+        <button className="Add-button Education-field" onClick={(e) => {
+          setEducationProfiles([
+            ...educationProfiles,
+            {
+              univ: univ,
+            }
+
+          ]);
+          setUniv('');
+          e.preventDefault();
+          console.log(educationProfiles);
+        }} type="button">Add</button>
         <button className="Clear-button Education-Field">Clear</button>
+      </div>
+      <div>
+        <p>Current Data:</p>
+        {
+          educationProfiles.map(profile => {
+            return (
+              <div key={profile.univ} className="Profile">{profile.univ}
+                <button type='button' className='Delete' onClick={() => {
+                  setEducationProfiles(
+                    educationProfiles.filter(p => p.univ !== profile.univ))
+                }}>Delete</button>
+              </div>)
+          })}
       </div>
     </div>
   )
@@ -50,6 +76,7 @@ function ExperienceField() {
         <button className="Add-button Experience-field">Add</button>
         <button className="Clear-button Experience-Field">Clear</button>
       </div>
+      <p>Current Data:</p>
     </div>
   )
 }
@@ -57,7 +84,7 @@ function ExperienceField() {
 function TextInput({ attrName, text }) {
   return (
     <>
-      <label for={attrName}>{text}</label>
+      <label htmlFor={attrName}>{text}</label>
       <input key={attrName} type="text" id={attrName} name={attrName} />
     </>
   )
@@ -71,7 +98,7 @@ function MainForm() {
         <EducationField></EducationField>
         <ExperienceField></ExperienceField>
       </div>
-      <button className='Download-button'>Download PDF</button>
+      <button className='Download-button' onSubmit={() => { }}>Download PDF</button>
     </form>
   );
 }
